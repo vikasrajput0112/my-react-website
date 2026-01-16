2063cdcac664330215486c75c4df2a806396905a
vikasrajput0112 <devops1@paisalo.in>
2026-01-16 17:36:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import Header from "./Header";
-import Sidebar from "./Sidebar";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
     return (
         <>
             <Header />
-
-            <div className="page-layout">
-                <Sidebar />
-
-                <main className="main-content">
-                    <div className="hero">
-                        <h1>Digital Solutions That Grow Your Business</h1>
-                        <p>
-                            We build reliable, scalable, and secure web solutions
-                            to help businesses succeed in the digital era.
-                        </p>
-                    </div>
-                </main>
-            </div>
-
+            <Home />
             <Footer />
         </>
     );
