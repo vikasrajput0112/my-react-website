b6105f188ad36c1663d0c983165c00a2584202eb
vikasrajput0112 <devops1@paisalo.in>
2026-01-16 18:42:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
     return (
-        <>
+        <Router>
             <Header />
-            <Home />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+
             <Footer />
-        </>
+        </Router>
     );
 }
 
