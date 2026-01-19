9c3633596ec2d2fdca52ad52bc5d78b206f2f73e
vikasrajput0112 <devops1@paisalo.in>
2026-01-19 12:55:56 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,19 @@ import Sidebar from "../components/Sidebar";
 
 const Home = () => {
     return (
-        <main className="page-layout">
+        <div className="page-layout">
             <Sidebar />
 
-            <section className="main-content">
-                <div className="hero">
-                    <h1>We -$provide LoAn That Grow Your Business</h1>
+            <main className="home-content">
+                <section className="home-hero">
+                    <h1>Welcome to Test-Website</h1>
                     <p>
-                        We build reliable, scalable, and secure web solutions
-                        to help businesses succeed in the digital era.
+                        We build simple, reliable, and scalable digital
+                        solutions with a clean and user-friendly experience.
                     </p>
-                </div>
-            </section>
-        </main>
+                </section>
+            </main>
+        </div>
     );
 };
 
