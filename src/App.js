9a999c87b45e6b7de182baa2dd560658c4f73ae2
vikasrajput0112 <devops1@paisalo.in>
2026-01-19 11:26:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
 function App() {
     return (
@@ -13,6 +16,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
 
             <Footer />
