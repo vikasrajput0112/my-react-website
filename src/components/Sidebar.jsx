738607a24d85fce96a151c369ad88f9cf53839cc
vikasrajput0112 <devops1@paisalo.in>
2026-01-19 11:41:21 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 const Sidebar = () => {
     return (
         <aside className="sidebar">
-            <h3>Modules</h3>
-            <ul>
-                <li>ABF</li>
-                <li>Dashboard</li>
-                <li>FI</li>
-                <li>HRMS</li>
+            <h3>Products</h3>
+
+            <ul className="sidebar-list">
+                <li>Mobile</li>
+                <li>Charger</li>
+                <li>Data Cable</li>
+                <li>Mouse</li>
+                <li>Mouse Pad</li>
             </ul>
         </aside>
     );
