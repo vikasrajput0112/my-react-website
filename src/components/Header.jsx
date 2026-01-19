c5e08e4ff6d051a4ad930d70ebca86ba10a40634
vikasrajput0112 <devops1@paisalo.in>
2026-01-19 10:51:06 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="header">
             <div className="container header-flex">
-                <div className="logo">My<span>Website</span></div>
+                <div className="logo">
+                    My<span>Website</span>
+                </div>
+
                 <nav className="nav">
-                    <a href="#">Home</a>
-                    <a href="#">About</a>
-                    <a href="#">Services</a>
-                    <a href="#">Contact</a>
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="#">Services</Link>
+                    <Link to="#">Contact</Link>
                 </nav>
             </div>
         </header>
