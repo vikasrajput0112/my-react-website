fd4671237e1fb660a3bb29ff8fa5e3c9f092b0b7
vikasrajput0112 <devops1@paisalo.in>
2026-01-19 12:55:26 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,8 @@
-import { Link } from "react-router-dom";
-
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer-nav">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/contact">Contact</Link>
-            </div>
-
             <p className="footer-text">
-                © 2026 My Website. All rights reserved.
+                © 2026 Test-Website. All rights reserved.
             </p>
         </footer>
     );
